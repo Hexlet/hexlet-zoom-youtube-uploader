a32d6003b15e7c399f4141f2fa54ad7797d5af5e
Andrey Moshkov <fakesupp@gmail.com>
2022-09-13 11:51:32 +0000

    init project

---
+++ terraform/hetzner.tf
@@ -0,0 +1,41 @@
+resource "hcloud_ssh_key" "andrey_m" {
+  name       = "andrey.m"
+  public_key = file("../files/ssh_keys/andrey.m")
+}
+
+resource "hcloud_ssh_key" "andrey_g" {
+  name       = "andrey.g"
+  public_key = file("../files/ssh_keys/andrey.g")
+}
+
+resource "hcloud_ssh_key" "sergey_m" {
+  name       = "sergey.m"
+  public_key = file("../files/ssh_keys/sergey.m")
+}
+
+resource "hcloud_ssh_key" "kirill_m" {
+  name       = "kirill.m"
+  public_key = file("../files/ssh_keys/kirill.m")
+}
+
+resource "hcloud_ssh_key" "stanislav_d" {
+  name       = "stanislav.d"
+  public_key = file("../files/ssh_keys/stanislav.d")
+}
+
+resource "hcloud_ssh_key" "kirill_c" {
+  name       = "kirill.c"
+  public_key = file("../files/ssh_keys/kirill.c")
+}
+
+resource "hcloud_server" "hexlet-zoom-youtube-uploader" {
+  name        = "zoom-youtube-uploader"
+  image       = "docker-ce"
+  server_type = "cx11"
+  location    = "hel1"
+  ssh_keys    = data.hcloud_ssh_keys.all_keys.ssh_keys.*.name
+}
+
+output "hexlet-zoom-youtube-uploader_ip_address" {
+  value = hcloud_server.hexlet-zoom-youtube-uploader.ipv4_address
+}
---
+++ terraform/vars.tf
@@ -0,0 +1,4 @@
+variable "hcloud_token" {
+  description = "Hetzner hcloud token"
+  sensitive   = true
+}
